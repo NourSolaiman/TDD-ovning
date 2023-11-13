f076919575f98e4f95d9917fb27f0e5baa8f2e3a
Tobias Andersson <tobiiasa@hotmail.com>
2023-11-13 14:03:51 +0100

    Added tests for FizzBuzz Kalkylator

---
+++ dotNet-TDD-Övning/FizzBuzz/Kalkylator.cs
@@ -0,0 +1,25 @@
+﻿namespace FizzBuzz
+{
+	public class Kalkylator
+	{
+		public static string FizzBuzzKalkyl(int number)
+		{
+			if(number % 3 == 0 && number % 5 == 0)
+			{
+				return "FizzBuzz";
+			}
+			else if (number % 3 == 0)
+			{
+				return "Fizz";
+			}
+			else if(number % 5 == 0)
+			{
+				return "Buzz";
+			}
+            else
+            {
+				return number.ToString();
+            }
+        }
+	}
+}
--- dotNet-TDD-Övning/FizzBuzzTest/UnitTest1.cs
+++ dotNet-TDD-Övning/FizzBuzzTest/UnitTest1.cs
@@ -1,11 +1,56 @@
+using FizzBuzz;
+
 namespace FizzBuzzTest
 {
 	public class UnitTest1
 	{
 		[Fact]
-		public void Test1()
+		public void When_FizzBuzzKalkyl_IsNotImplemented()
 		{
-			 
-		}
-	}
+            // Arrange
+            int randomNummer = 0;
+
+            // Act
+            Kalkylator.FizzBuzzKalkyl(randomNummer);
+
+            // Testet kommer att bli grönt då ett NotImplementedException kastas
+            
+        }
+        [Fact]
+        public void When_FizzBuzzKalkyl_Fizz()
+        {
+            //Arrange
+            int number = 3;
+
+            //Act
+            var result = Kalkylator.FizzBuzzKalkyl(number);
+
+            //Assert
+            Assert.Equal("Fizz", result);
+        }
+        [Fact]
+        public void When_FizzBuzzKalkyl_Buzz()
+        {
+            //Arrange
+            int number = 5;
+
+            //Act
+            var result = Kalkylator.FizzBuzzKalkyl(number);
+
+            //Assert
+            Assert.Equal("Buzz", result.ToString());
+        }
+        [Fact]
+        public void When_FizzBuzzKalkyl_FizzBuzz()
+        {
+            //Arrange
+            int number = 15;
+
+            //Act
+            var result = Kalkylator.FizzBuzzKalkyl(number);
+
+            //Assert
+            Assert.Equal("FizzBuzz", result.ToString());
+        }
+    }
 }
